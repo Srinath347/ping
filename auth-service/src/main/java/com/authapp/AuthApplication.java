e26a410c23564b2d09a6381025cce2c289ad6160
shejo mathew <shejomathew@shejos-MacBook-Air.local>
2022-12-05 03:04:04 -0600

    Auth Service

---
+++ auth-service/src/main/java/com/authapp/AuthApplication.java
@@ -0,0 +1,14 @@
+package com.authapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+//		AuthUtil authUtil = new AuthUtil();
+//		authUtil.userSignIn();
+	}
+}
