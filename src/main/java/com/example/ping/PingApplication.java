5af8d5b3424d25c31789b9601448fece97820d9c
Srinath347 <srinathm.777@gmail.com>
2022-12-05 21:37:27 -0600

    Key generation

--- src/main/java/com/example/ping/PingApplication.java
+++ src/main/java/com/example/ping/PingApplication.java
@@ -4,12 +4,32 @@ import com.example.ping.util.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileOutputStream;
+import java.security.*;
+
 @SpringBootApplication
 public class PingApplication {
 
 	public static void main(String[] args) {
 		Client.getAllUsers();
+//		keyGeneration();
 		SpringApplication.run(PingApplication.class, args);
+	}
+
+	private static void keyGeneration() throws Exception {
+		String publicKeyFilePath = "/Users/srinath/Desktop/ping/keys/srinath.key";
+		String privateKeyFilePath = "/Users/srinath/Desktop/srinath.key";
+		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(2048);
+		KeyPair pair = generator.generateKeyPair();
+		PublicKey publicKey = pair.getPublic();
+		try (FileOutputStream fos = new FileOutputStream(publicKeyFilePath)) {
+			fos.write(publicKey.getEncoded());
+		}
+		PrivateKey privateKey = pair.getPrivate();
+		try (FileOutputStream fos = new FileOutputStream(privateKeyFilePath)) {
+			fos.write(privateKey.getEncoded());
+		}
 	}
 
 }
